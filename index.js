331364f3be9afdb6d73571d0879c34e9362145d5
Prakriti <prakriti_bansal@yahoo.com>
2018-03-21 12:34:05 +0530

    Some Modidications

--- index.js
+++ index.js
@@ -2,14 +2,32 @@
 
 const Alexa = require('alexa-sdk');
 
-const handlers = {
-  "LaunchRequest": () => {
-
-  }
-}
-
 module.exports.handler = (event, context) => {
   const alexa = Alexa.handler(event, context);
+  alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
   alexa.registerHandlers(handlers);
   alexa.execute();
 }
+
+const handlers = {
+    'FindItemIntent' : function() {
+        this.response.speak('What would you like to find?');
+        this.emit(':ask', ':responseReady');
+    },
+
+    'StoreItemIntent' : function() {
+        this.response.speak('What would you like to store?');
+        this.emit(':responseReady');
+    },
+
+    'StoreEventItem' : function() {
+        //build response first using responseBuilder and then emit
+        this.response.speak('Hello World!');
+        this.emit(':responseReady');
+    },
+    'ListEventItem' : function() {
+        //build response first using responseBuilder and then emit
+        this.response.speak('Hello World!');
+        this.emit(':responseReady');
+    }
+};
