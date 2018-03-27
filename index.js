212742ff14cd815166fa7570ce9c9bd8023b5342
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-03-28 00:14:01 +0530

    code repositioned and optimized

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const Alexa = require('alexa-sdk');
 const awsSDK = require('aws-sdk');
 const thesaurus = require('thesaurus-com');
-//todo: es6-promisify is now out with a newer version, so update your code with the newer version of the API.
-//todo: const promisify = require('es6-promisify');
-
 const itemsTableName = 'Items';
 const timeStampTableName = 'TimeStamp';
 const activeListTableName = 'ActiveList';
 const documentClient = new awsSDK.DynamoDB.DocumentClient();
 const documentClientNew = new awsSDK.DynamoDB.DocumentClient();
 let activeListFetchedStatus = false;
-let activeList = []; //todo: this would behave as out cache active List
 
+//this activeList is filled up at the beginning of the program and emptied at the exit of the program
+let activeList = [];
+
+//function to fetch activeList at the beginning of the start of Alexa
 function fetchActiveListAndCache(userId) {
   const params = {
     TableName: activeListTableName,
@@ -22,7 +22,7 @@ function fetchActiveListAndCache(userId) {
   };
   documentClient.get(params, function (err, data) {
     if(err) {
-      console.log('oops something went wrong', err);
+      console.log("oops! activeList couldn't be fetched", err);
     } else {
       console.log('activeList has been cached');
       activeListFetchedStatus = true;
@@ -31,8 +31,48 @@ function fetchActiveListAndCache(userId) {
   })
 }
 
-function filterSynonyms(synonyms) {
+//stores the activeList back into the ActiveList table after the program comes to a halt
+function storeActiveList(userId) {
+  const params = {
+    TableName: activeListFetchedStatus,
+    Item: {
+      "userId": userId,
+      "activeList": activeList
+    }
+  };
+  documentClient.put(params, function (err, data) {
+    if(err) {
+      console.log("activeList couldn't be stored");
+    } else {
+      console.log("activeList has been stored", data);
+    }
+  })
+}
 
+function moveFromActiveListToDB(userId, item) {
+  const params = {
+    TableName: itemsTableName,
+    Item:{
+      "itemName-userId": item.itemName + "-" + userId,
+      "userId": userId,
+      "itemName": item.itemName,
+      "locationName": item.locationName
+    }
+  };
+
+  documentClient.put(params, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Added item to the database:", JSON.stringify(data, null, 2));
+    }
+  });
+}
+
+//function to filter out the unnecessary synonyms
+function filterSynonyms(synonyms) {
+  //todo: this function is to be implemented
+  return synonyms;
 }
 
 const handlers = {
@@ -72,6 +112,7 @@ const handlers = {
     let searchFlag = false;
     let requiredSynonyms = [];
 
+    //search in activeList with itemName
     for (let activeMember in activeList) {
       if(activeMember[itemName]) {
         emitCopy(":tell", `your ${itemName} is located at ${activeMember[itemName]}`);
@@ -79,6 +120,7 @@ const handlers = {
         break;
       }
     }
+    //search in activeList with synonym
     if(!searchFlag) {
       console.log('Attempting to read data of synonyms in activeList');
       const synonyms = thesaurus.search(itemName).synonyms;
@@ -88,13 +130,14 @@ const handlers = {
         for (let activeMember in activeList) {
           if(activeMember[synonym]) {
             //todo: user has to confirm that this is what he requires, setup dialog model
-            emitCopy(":tell", `your ${synonym} is located at ${activeMember[itemName]}`);
+            emitCopy(":tell", `your ${synonym} is located at ${activeMember[synonym]}`);
             searchFlag = true;
             break;
           }
         }
       });
     }
+    //search in Items table using ItemName
     if(!searchFlag) {
       console.log('Attempting to read data in Items table');
       let params = {
@@ -112,6 +155,7 @@ const handlers = {
           if(data.Item) {
             emitCopy(":tell", `you can find your ${data.Item.itemName} at ${data.Item.locationName}`)
           } else {
+            //search in Items table on the basis of synonym
             console.log('Attempting to read data of synonyms in Items table');
             requiredSynonyms.forEach(function (synonym) {
               params.Key = {
@@ -124,6 +168,8 @@ const handlers = {
                 } else {
                   if(data.Item) {
                     emitCopy(":tell", `you can find your ${data.Item.itemName} at ${data.Item.locationName}`)
+                  } else {
+                    //todo: now check history @shikhar and @prakriti
                   }
                 }
               });
@@ -134,13 +180,31 @@ const handlers = {
       });
     }
   },
+
+  'AMAZON.CancelIntent': function () {
+    const { userId } = this.event.session.user;
+    storeActiveList(userId);
+  },
+
+  'AMAZON.StopIntent': function () {
+    const { userId } = this.event.session.user;
+    storeActiveList(userId);
+  },
+
+  'LaunchRequest':  function () {
+    //todo: ask Shikhar or Prakriti to design this
+    // Prakriti had already used this in one of her PR's
+  },
   
   'StoreItemIntent': function () {
-    let emitCopy = this.emit;
     const { userId } = this.event.session.user;
     const { slots } = this.event.request.intent;
-    
-    fetchExistingTimeStamp(userId);
+
+    //fetch activeList if not yet
+    if(!activeListFetchedStatus) {
+      fetchActiveListAndCache(userId);
+    }
+
     // name of the item
     if (!slots.Item.value) {
       const slotToElicit = 'Item';
@@ -183,91 +247,16 @@ const handlers = {
       const repromptSpeech = 'Please give me a location where the item is stored.';
       return this.emit(':elicitSlot', slotToElicit, speechOutput, repromptSpeech);
     }
-    
-    let params = {
-      TableName: itemsTableName,
-      Item:{
-        "itemName-userId": slots.Item.value + "-" + userId,
-        "userId": userId,
-        "itemName": slots.Item.value,
-        "locationName": slots.Place.value
-      }
-    };
-    //todo: (SHM's task) => Prakriti's method of first searching and then storing. But this will be done once the database table design has been finalized.
-    //todo: (not required for now)Promisify to be used here(check how to use the new API)
-    documentClient.put(params, function(err, data) {
-      if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        emitCopy(':tell', "oops! something went wrong");
-      } else {
-        console.log("Added item:", JSON.stringify(data, null, 2));
-        emitCopy(':tell', `your ${slots.Item.value} has been stored at ${slots.Place.value}`);
-      }
+    activeList.push({
+      itemName: slots.Item.value,
+      locationName: slots.Place.value
     });
+    if(activeList.length > 10) {
+      moveFromActiveListToDB(userId, activeList.shift());
+    }
+    this.emit(":tell", `your ${slots.Item.value} has been stored at ${slots.Place.value}`)
   }
 };
-
-function fetchExistingTimeStamp(userId) {
-  const timeStampParams = {
-    TableName: timeStampTableName,
-    Key: {
-      "userId": userId
-    }
-  };
-  documentClientNew.get(timeStampParams, function (err, data) {
-    if (err) {
-      console.error("Time stamp not working", JSON.stringify(err, null, 2));
-      checkRenew(false, userId);
-    } else {
-      console.log("Time Stamp found:", JSON.stringify(data, null, 2));
-      checkRenew(data, userId);
-    }
-  });
-}
-
-function checkRenew(data, userId) {
-  const date = new Date();
-  const currentTimeStamp = date.getTime();
-  if(!data.Item) {
-    let params = {
-      TableName: timeStampTableName,
-      Item:{
-        "userId": userId,
-        "timestamp": currentTimeStamp
-      }
-    };
-    
-    documentClientNew.put(params, function(err, data) {
-      if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-      } else {
-        console.log("Added item:", JSON.stringify(data, null, 2));
-      }
-    });
-  } else {
-    if(currentTimeStamp - data.Item.timestamp >= 86400000) {
-      let params = {
-        TableName: timeStampTableName,
-        Item:{
-          "userId": userId,
-          "timestamp": currentTimeStamp
-        }
-      };
-      documentClientNew.put(params, function(err, data) {
-        if (err) {
-          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-          console.log("Added item:", JSON.stringify(data, null, 2));
-          copyActiveList();
-        }
-      });
-    }
-  }
-}
-
-function copyActiveList() {
-  //todo: update items database with the activeList
-}
 
 exports.handler = function (event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
